78fd3b853ece543e3a4ec95b7a0626b0a3e06439
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2023-02-14 09:53:04 +0530

    Update poweOfTwo.java

--- Easy/poweOfTwo.java
+++ Easy/poweOfTwo.java
@@ -1,12 +1,7 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-      if(n==0){
-          return false;
-        
-      }
-        return (int)(Math.ceil((Math.log(n) / Math.log(2))))
-            == (int)(Math.floor(
-                ((Math.log(n) / Math.log(2)))));
+   public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return (n & (n-1)) == 0;
     }
     
 }
